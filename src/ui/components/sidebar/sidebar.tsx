60c42b686c895f0005a3e2cd85c486c6870df2a3
May <40745122+SomewhatMay@users.noreply.github.com>
2024-07-16 13:00:57 -0400

    setup

--- src/ui/components/sidebar/sidebar.tsx
+++ src/ui/components/sidebar/sidebar.tsx
@@ -4,6 +4,10 @@ import { useRem } from "ui/hooks/use-rem";
 export function Sidebar() {
 	const rem = useRem();
 
+	useEffect(() => {
+		print("Mounted sidebar");
+	});
+
 	return (
 		<frame Size={new UDim2(0, rem(50), 1, 0)} BackgroundTransparency={0.5}></frame>
 	);

e4e63ca98c5fd4adf2e12aa2e0da62e00e619bcb
May <40745122+SomewhatMay@users.noreply.github.com>
2024-08-05 15:09:45 -0400

    only major version is needed inside FileData as that is what determines compatibility

--- src/io/reader/validate-data.ts
+++ src/io/reader/validate-data.ts
@@ -5,5 +5,5 @@ import { $terrify } from "rbxts-transformer-t-new";
 const fileDataCheck = $terrify<FileData>();
 
 export function validateData(data: unknown) {
-	return fileDataCheck(data) && t.match("^v%d+%.%d+%.%d+$")(data.Version);
+	return fileDataCheck(data);
 }
--- src/io/writer/write-file.ts
+++ src/io/writer/write-file.ts
@@ -1,16 +1,16 @@
 import { PointCollection } from "store/editor-slice";
 import { serializeCollection } from "./serialize-collection";
-import { getVersion } from "ui/util/get-version";
+import config from "config";
 
 export interface FileData {
-	Version: string;
+	Version: number;
 	FillBounds: boolean;
 	PointsData: PointCollection;
 }
 
 export function writeFile(fileObject: ModuleScript, data: Omit<FileData, "Version">) {
 	let fileData = "return {\n";
-	fileData += "\tVersion = '" + getVersion() + "',\n";
+	fileData += "\tVersion = " + config.version.major + ",\n";
 	fileData += "\tFillBounds = " + data.FillBounds + ",\n";
 	fileData += serializeCollection(data.PointsData);
 	fileData += "\n}";
